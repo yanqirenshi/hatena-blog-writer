9682be0fc313a6ea731122e7c261f8655568ccf5
yanqirenshi <yanqirenshi@gmail.com>
2018-07-24 10:38:21 +0900

    rm auto-mode-alist

--- src/mode/major.el
+++ src/mode/major.el
@@ -1,13 +1,15 @@
 ;;; -*- coding: utf-8; lexical-binding: t -*-
 
-(define-derived-mode hatena-blog-writer nil "hatena-blog-writer" "はてなブログ執筆モード")
+(define-derived-mode
+  hbw                       ;; 作りたいモード名
+  nil                       ;; 親モード名
+  "hbw"                     ;; モード行に表示するモード名の文字列
+  "はてなブログ執筆モード") ;; このモードの説明文字列
 
-;; *hatena-blog: yanqirenshi*
-;; これ効いていない。。。
-(add-to-list 'auto-mode-alist '("\*hatena-blog: .*\*'" . hatena-blog-writer))
 
-(defun hatena-blog-writer-hook ()
-  (let ((b (get-buffer-create "*hatena-blog: TEST*")))
+(defun hatena-blog-writer ()
+  (interactive)
+  (let ((b (hatena-blog-writer-ensure-major-mode-buffer)))
     (switch-to-buffer (buffer-name b))
     (hatena-blog-writer-open-major-mode-buffer b)
     (message "Enjoy write blog!")))
